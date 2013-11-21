d5a87f11cdd757fc1d7528a91b50dbaccccaf9ba
Aurelien GIRY <agi@emakina.fr>
2013-11-21 18:05:09 +0100

    surcharge des templates form

---
+++ src/Aml/Bundle/EvenementsBundle/Entity/EvenementBlog.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Andromeda
+ *
+ * NOTICE OF LICENSE
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Andromeda to newer
+ * versions in the future.
+ *
+ * @category Andromeda
+ * @package Andromeda_${NAME}
+ * @copyright Copyright (c) 2011-2013 Brady. (http://www.brady.com)
+ */ 
