bbe797c2496abd80bfe2aef9f42e9976255a1b1c
Aurélien GIRY <aurelien.giry@gmail.com>
2015-01-29 14:17:40 +0100

    clean composer.json

--- src/Aml/Bundle/WebBundle/Resources/public/ckeditor/ckeditor.php
+++ src/Aml/Bundle/WebBundle/Resources/public/ckeditor/ckeditor.php
@@ -23,7 +23,5 @@
  * @endcode
  */
 
-if ( !function_exists('version_compare') || version_compare( phpversion(), '5', '<' ) )
-	include_once( 'ckeditor_php4.php' ) ;
-else
-	include_once( 'ckeditor_php5.php' ) ;
+
+include_once( 'ckeditor_php5.php' ) ;
