3c5b6939536c8f30a8e9793743a97624aeea8df1
Aurélien GIRY <aurelien.giry@gmail.com>
2018-12-16 16:53:48 +0100

    Global: Install php-cs-fixer on pre-commit

---
+++ hooks/pre-commit.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo "php-cs-fixer start"
+
+PHP_CS_FIXER="./vendor/bin/php-cs-fixer"
+
+if [ -x $PHP_CS_FIXER ]; then
+    if git diff --cached --name-only --diff-filter=ACMRTUXB | grep -q '\.php$'; then
+        STAGED_FILES=$(git diff --cached --name-only --diff-filter=ACMRTUXB | grep '\.php$')
+        $PHP_CS_FIXER fix --verbose --allow-risky=yes --config=.php_cs -- ${STAGED_FILES[@]};
+        git add ${STAGED_FILES[@]}
+    fi
+else
+    echo ""
+    echo "Please install php-cs-fixer, e.g.:"
+    echo ""
+    echo "    composer global require friendsofphp/php-cs-fixer"
+    echo " or composer require --dev friendsofphp/php-cs-fixer"
+    echo ""
+fi
