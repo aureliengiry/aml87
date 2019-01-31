2bfb5008c3d55bccbe336cc013e81f8d6abecaa1
Aurélien GIRY <aurelien.giry@gmail.com>
2019-01-31 13:55:48 +0100

    Assets: clean JS & CSS

--- assets/js/members-area.js
+++ assets/js/members-area.js
@@ -1,8 +1,10 @@
 // loads the jquery package from node_modules
-var $ = require("jquery");
+const $ = require("jquery");
 
 require("bootstrap");
 require("mdbootstrap");
+
+require("../css/members-area.sass");
 
 $(document).ready(function() {
     $('[data-toggle="popover"]').popover();
