4816e62d60565fe831b52fce0a47a02a9c27115b
Aurélien GIRY <aurelien.giry@gmail.com>
2022-02-14 10:52:20 +0100

    Docker: update docker for dev light/full

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,39 @@
 // webpack.config.js
-const Encore = require('@symfony/webpack-encore');
+const Encore = require('@symfony/webpack-encore')
 
 Encore
-// directory where all compiled assets will be stored
-    .setOutputPath('public/build/')
+  // directory where all compiled assets will be stored
+  .setOutputPath('public/build/')
 
-// what's the public path to this directory (relative to your project's document root dir)
-    .setPublicPath('/build')
+  // what's the public path to this directory (relative to your project's document root dir)
+  .setPublicPath('/build')
 
-// empty the outputPath dir before each build
-    .cleanupOutputBeforeBuild()
+  // empty the outputPath dir before each build
+  .cleanupOutputBeforeBuild()
 
-// will output as web/build/app.js
-    .addEntry('app', './assets/js/app.js')
-    .addEntry('members-area', './assets/js/members-area.js')
+  // will output as web/build/app.js
+  .addEntry('app', './assets/js/app.js')
+  .addEntry('members-area', './assets/js/members-area.js')
 
-// will output as web/build/main.css
-// .addStyleEntry('main-css', './assets/css/app.sass')
+  // will output as web/build/main.css
+  // .addStyleEntry('main-css', './assets/css/app.sass')
 
-// allow sass/scss files to be processed
-// .enableSassLoader(function(sassOptions) {}, {
-//     resolve_url_loader: false
-// })
-    .enableSassLoader()
+  // allow sass/scss files to be processed
+  // .enableSassLoader(function(sassOptions) {}, {
+  //     resolve_url_loader: false
+  // })
+  .enableSassLoader((options) => {
+    options.implementation = require('sass')
+  })
 
-// allow legacy applications to use $/jQuery as a global variable
-    .autoProvidejQuery()
+  // allow legacy applications to use $/jQuery as a global variable
+  .autoProvidejQuery()
 
-    .enableSourceMaps(!Encore.isProduction())
+  .enableSourceMaps(!Encore.isProduction())
 
-// create hashed filenames (e.g. app.abc123.css)
-// .enableVersioning()
-    .disableSingleRuntimeChunk();
+  // create hashed filenames (e.g. app.abc123.css)
+  // .enableVersioning()
+  .disableSingleRuntimeChunk()
 
 // export the final configuration
-module.exports = Encore.getWebpackConfig();
+module.exports = Encore.getWebpackConfig()
